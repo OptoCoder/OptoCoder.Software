6387bc98c941b84c239447cb01395bb18d9a6898
rahiraihan <rahi2721@gmail.com>
2019-07-11 22:55:14 +0600

    Add project files.

---
+++ OptoCoder.Software/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OptoCoder.Software.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
