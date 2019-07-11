95fa0785717a3e00e8bace235b87c8ac78af447e
rahiraihan <rahi2721@gmail.com>
2019-07-12 00:05:21 +0600

    changes

---
+++ OptoCoder.Software.Repository/accountingRepository/IproductAddCategory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptoCoder.Software.Repository.accountingRepository
+{
+    interface IproductAddCategory
+    {
+    }
+}
